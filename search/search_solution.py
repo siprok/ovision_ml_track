58a7852652e8c8d1de46360f2af2c7abefbda5d6
holyfame <perchuk.sa@phystech.edu>
2022-04-16 01:14:50 +0300

    Implement raw faiss solution

--- search/search_solution.py
+++ search/search_solution.py
@@ -1,6 +1,12 @@
-import numpy as np 
-from .search import Base
+import pickle, os, time, gdown
+import numpy as np
+
+from tqdm import tqdm
 from typing import List, Tuple
+from config import Config as cfg
+from .search import Base
+
+import faiss
 
 class SearchSolution(Base):
 
@@ -11,10 +17,42 @@ class SearchSolution(Base):
         pass
 
     def set_base_from_pickle(self):
-        pass
+        if not os.path.isfile(self.data_file):
+            if not os.path.isdir('./data'):
+                os.mkdir('./data')
+            gdown.download(self.data_url, self.data_file, quiet=False)
+
+        with open(self.data_file, 'rb') as f:
+            data = pickle.load(f)
+
+        self.reg_matrix = [None] * len(data['reg'])
+        self.ids = {}
+        for i, key in enumerate(data['reg']):
+            self.reg_matrix[i] = data['reg'][key][0][None]
+            self.ids[i] = key
+
+        self.reg_matrix = np.concatenate(self.reg_matrix, axis=0)
+        self.pass_dict = data['pass']
+
+        self.set_up_faiss()
+
+    def set_up_faiss(self):
+        dim = 512
+        # print("... train index ...")
+        self.index = faiss.index_factory(dim, 'IVF1000,Flat')
+        self.index.train(self.reg_matrix.astype('float32'))
+        # print("... add vectors to index ...")
+        self.index.add(self.reg_matrix.astype('float32'))
+        self.index.nprobe = 32
 
     def search(self, query: np.array) -> List[Tuple]:
-        pass
+        topn = 1
+        return self.index.search(np.array([query]).astype('float32'), topn)[1] # Distance, Index
 
     def cos_sim(self, query: np.array) -> np.array:
-        pass
+        return np.dot(self.reg_matrix, query)
+
+    def insert(self, feature: np.array) -> None:
+        self.reg_matrix = np.concatenate(self.reg_matrix, feature, axis=0)
+        # TODO: создать очередь элементов. Обновлять faiss только когда накопится критическая масса
+        self.set_up_faiss()
